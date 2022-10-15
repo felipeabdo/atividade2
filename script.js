9417063f4c4534e2bfd73bad52a7d47d4e1b810e
Felipe Barroso Abdo <felipe.abdo@gmail.com>
2022-10-15 17:24:58 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function meuMenu() {
+    var abrirAside = document.querySelector("aside");
+    abrirAside.classList.toggle("ativoAside");
+
+    var abrirMain = document.querySelector("main");    
+    abrirMain.classList.toggle("ativoMain")
+}
