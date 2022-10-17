715d15af37b5e869e811cfa58f9d6f42d1877c93
Felipe Barroso Abdo <felipe.abdo@gmail.com>
2022-10-17 09:51:14 -0300

    Sem vazar

--- script.js
+++ script.js
@@ -4,4 +4,10 @@ function meuMenu() {
 
     var abrirMain = document.querySelector("main");    
     abrirMain.classList.toggle("ativoMain")
+
+    var deslMenu = document.querySelector("label");
+    deslMenu.classList.toggle("ativoMenu")
+
+    var deslLogo = document.querySelector("#logo");
+    deslLogo.classList.toggle("ativoLogo")
 }
